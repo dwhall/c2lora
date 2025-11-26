87b9a0a155f8d62d84c1d27c13ee601ee8503f3c
Joshua Zimmerman <joshua.curtis.zimmerman@gmail.com>
2025-11-25 23:34:10 -0600

    Change ugly emit block to pretty? type based template

--- src/startup.nim
+++ src/startup.nim
@@ -1,15 +1,24 @@
 proc main
 
+template sectionVar*(section: string, name: untyped, typ: typedesc, init: string): untyped =
+  var name {.exportc: astToStr(name), codegenDecl:
+    "__attribute__((section(\"" & section & "\")))\n" &
+    "__attribute__((used))\n" &
+    "$# $# = " & init
+  .}: typ
+
+type
+  VectorTable = object
+    stackPointer: uint32
+    resetHandler: proc() {.noconv.}
+
 # Arm cortex m4 architecture uses Reset_Handler as an
 # entry point.
-proc Reset_Handler() {.exportc.} =
+proc Reset_Handler() {.exportc, noconv.} =
   main()
   while true: discard
 
-{.emit: """
-__attribute__((section(".isr_vector")))
-void (* const vectors[])(void) = {
-    (void (*)(void))(0x20040000),  // Stack pointer
-    Reset_Handler,
-};
-""".}
+sectionVar(".isr_vector", vectorTable, VectorTable, """{
+  .stackPointer = 0x20040000,
+  .resetHandler = Reset_Handler
+}""")
